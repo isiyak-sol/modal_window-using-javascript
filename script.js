0f77ba5ab3dfc8b6b48973e7f5412a2d4fd89d11
Isiyak-sol <isiyakjit2021@gmail.com>
2022-12-27 04:43:38 -0800

    App Updated

--- script.js
+++ script.js
@@ -19,3 +19,55 @@ overlay.addEventListener('click', closeModal)
 
 
 // Calculator Part
+const additionBtn = document.querySelector('.add');
+const substractionBtn = document.querySelector('.sub');
+const multiplicationBtn = document.querySelector('.mul');
+const divisionBtn = document.querySelector('.div');
+
+// Getting Variables
+const variables = function() {
+     
+}
+
+// Getting Result
+const totalValue = function(total) {
+      document.querySelector('.result').textContent = total;
+}
+
+// Addition
+const add = function() {
+// 1. Getting inputs from the user
+      const num1 = Number(document.querySelector('.num1').value);
+      const num2 = Number(document.querySelector('.num2').value);
+      const a = num1 + num2 ;
+      totalValue(a);
+}
+
+const sub = function() {
+      // 1. Getting inputs from the user
+      const num1 = Number(document.querySelector('.num1').value);
+      const num2 = Number(document.querySelector('.num2').value);
+      const a = num1 - num2 ;
+      totalValue(a);
+}
+
+const mul = function() {
+      // 1. Getting inputs from the user
+      const num1 = Number(document.querySelector('.num1').value);
+      const num2 = Number(document.querySelector('.num2').value);
+      const a = num1 * num2 ;
+      totalValue(a);
+}
+
+const div = function() {
+      // 1. Getting inputs from the user
+      const num1 = Number(document.querySelector('.num1').value);
+      const num2 = Number(document.querySelector('.num2').value);
+      const a = num1 / num2 ;
+      totalValue(a);
+}
+
+additionBtn.addEventListener('click', add)
+substractionBtn.addEventListener('click', sub)
+multiplicationBtn.addEventListener('click', mul)
+divisionBtn.addEventListener('click', div)
